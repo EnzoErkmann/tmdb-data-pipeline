e520f2dcb9564100994ce5597cc96e680d0e2bec
Enzo <enzo.erkmann5@gmail.com>
2026-07-04 20:08:38 -0300

    feat: add mart_release_seasonality model to analyze movie performance by release month

--- dbt/tmdb_project/models/marts/mart_release_seasonality.sql
+++ dbt/tmdb_project/models/marts/mart_release_seasonality.sql
@@ -5,6 +5,20 @@ with movies as (
 seasonality as (
     select
         extract(month from release_date) as release_month,
+        case extract(month from release_date)
+            when 1 then 'January'
+            when 2 then 'February'
+            when 3 then 'March'
+            when 4 then 'April'
+            when 5 then 'May'
+            when 6 then 'June'
+            when 7 then 'July'
+            when 8 then 'August'
+            when 9 then 'September'
+            when 10 then 'October'
+            when 11 then 'November'
+            when 12 then 'December'
+        end as month_name_en,
         count(movie_id) as total_movies,
         avg(revenue) as avg_revenue,
         avg(popularity) as avg_popularity,
